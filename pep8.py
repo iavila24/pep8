c880b62348655346d31b4eb891e3b56b7bbbd7cc
Cheems <iavila24@uabc.edu.mx>
2023-05-02 19:53:14 -0700

    Archivo formateado con PEP8

--- pep8.py
+++ pep8.py
@@ -1,11 +1,21 @@
-class user():
+# Clases con CamelCase
+# Identacion: 4 espacios (no tabs)
+# Parametros separados por comas separados por 1 espacio
+# Variables con snake_case
+# 2 Espacios entre operadores
+# Terminar scripts con un salto de linea
+# https://pep8.org/
 
- def __init__(self,username,password=''):
+class UserAdmin():
 
- self.username = username
- self.password = password
 
- def set_password(self):
-    pass
+    def __init__(self, username, password = ''):
+        self.username = username
+        self.password = password
 
-codyuser = user('Cody')
+
+    def set_password(self):
+        pass
+
+
+cody_user = UserAdmin('Cody')
